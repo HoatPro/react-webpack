a4498ecf3ff5f82230ddd864ee429929f1ba6194
HoatPro <nguyenvanhoat.ict@gmail.com>
2018-08-24 15:10:30 +0700

    them file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import sum from './math';
+console.log('Setup React Webpack Babel');
+console.log('hello');
+console.log('Chao ban');
+const total=sum(35,6);
+console.log(total);
+document.write(total);
+
+//module.hot.accept();
